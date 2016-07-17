b70f07361d9296b61d0b051c7ab8bbe0eb8eaa8a
ebenezer <beni522@gmail.com>
2016-07-16 22:35:13 -0500

    better performance in feature runtime, less 4 seconds

--- fare_estimator.js
+++ fare_estimator.js
@@ -4,32 +4,15 @@
  * (cost_per_minute)(ride_time) + (cost_per_mile)(distance_trip)
  */
 
-var fares = {
-	getCars: function() {
-		return [{
-			essential: {
-				cost_per_minute: 0.2,
-				cost_per_mile:1.1
-			},
-			executive: {
-				cost_per_minute: 0.35,
-				cost_per_mile:1.8
-			},
-			suv: {
-				cost_per_minute: 0.4,
-				cost_per_mile: 2.3
-			},
-			extra: {
-				cost_per_minute: 0.45,
-				cost_per_mile: 3.5
-			},
-			travel_trip: {
-				cost_per_minute: 0.50,
-				cost_per_mile: 4.0
-			}
-		}];
-	},
+var cars = [
+	{ feature: 'essential', cost_per_minute: 0.2, cost_per_mile:1.1 },
+	{ feature: 'executive', cost_per_minute: 0.35, cost_per_mile:1.8 },
+	{ feature: 'suv', cost_per_minute: 0.4, cost_per_mile: 2.3 },
+	{ feature: 'travel_trip', cost_per_minute: 0.45, cost_per_mile: 3.5 },
+	{ feature: 'family', cost_per_minute: 0.55, cost_per_mile: 4.5 }
+];
 
+var fares = {
 	hasErrors: function(ride_time, distance_trip) {
 		var errors = [];
 
@@ -64,83 +47,54 @@ var fares = {
 		return errors;
 	},
 
-	parseToMiles: function(distance_trip) {
-		return distance_trip * 0.621371192237334;
-	},
-
 	computeEstimateFare: function(ride_time, distance_trip) {
 		var errors = this.hasErrors(ride_time, distance_trip);
-		var results = {
-			message: null,
-			result: [],
-			error: false
+
+		if (errors.length) {
+			return {
+				messages: errors,
+				error: true
+			};
 		};
 
-		try {
-			if (errors.length) throw errors;
+		var estimates = [];
+		for (var x = 0; x < cars.length; x++) {
+			var fare = (ride_time * cars[x].cost_per_minute) + (distance_trip * cars[x].cost_per_mile);
+			estimates.push({
+				feature: cars[x].feature,
+				fare: fare.toFixed(1)
+			});
+		};
 
-			var distance_in_miles = distance_trip; //this.parseToMiles(distance_trip);
-			var cars = this.getCars();
-			var estimates = [];
-			var types = Object.keys(cars[0]);
-
-			if (types) {
-				for (var x = 0; x < types.length; x++) {
-					var type = types[x];
-
-					cars.forEach(function(car) {
-						var found = car[type];
-
-						if (found) {
-							var compute_fare_estimate = (ride_time * found.cost_per_minute) + (distance_in_miles * found.cost_per_mile);
-							estimates.push({
-								'type': type,
-								'price': parseFloat(compute_fare_estimate.toFixed(2))
-							});
-						};
-					})
-				};
-
-				results.result = estimates
-			};
-
-		} catch(errors) {
-			results.message = 'could not be computed fare estimates because some errors are present';
-			results.result = errors;
-			results.error = true;
-		}
-
-		return results;
+		return estimates
 	}
 };
 
+console.log('start runtime business logic\n');
+console.time('runtime');
+
+var showing_records = function(error, records) {
+	for (var x = 0; x < records.length; x++) {
+		var value = (error ? "Error: " + records[x] : "fare estimate for " + records[x].feature + ': ' + records[x].fare);
+		console.log(value);
+	};
+
+	console.timeEnd("runtime");
+	console.log('end runtime business logic\n');
+}
 /**
  * Parameters: function(ride_time, distance_trip)
  * both parameters are numeric.
- * ride_time: minutes
  * distance_trip: miles
+ * ride_time: minutes
  *
 */
-var distance_trip = 50;
+var distance_trip = 17;
 var ride_time = 40;
+var estimate = fares.computeEstimateFare(ride_time, distance_trip);
 
-var estimates = fares.computeEstimateFare(ride_time, distance_trip);
-var has_error = estimates['error'];
+if (estimate.error) {
+	return showing_records(true, estimate.messages);
+};
+showing_records(false, estimate);
 
-if (!has_error) {
-	console.log('SUCCESS');
-	console.log('Trip distance:', distance_trip, 'miles');
-	console.log('Ride time:', ride_time, 'minutes');
-	console.log('\n');
-} else {
-	console.log('FAILS', '(' + estimates.message + ')');
-}
-
-estimates.result.forEach(function(result) {
-	if (has_error) {
-		console.log('Error ===>', result);
-	} else {
-		var value = 'fare estimate for ' + result['type'] + ': ' + result.price;
-		console.log(value);
-	}
-});
